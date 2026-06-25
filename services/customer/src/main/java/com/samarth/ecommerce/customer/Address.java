beb9f92610667e046ec780fe6969d08fe9aac8ca
2StrokeLover <samarthtowar89@gmail.com>
2026-06-25 23:56:58 +0530

    Initial commit

---
+++ services/customer/src/main/java/com/samarth/ecommerce/customer/Address.java
@@ -0,0 +1,16 @@
+package com.samarth.ecommerce.customer;
+
+import lombok.*;
+import org.springframework.validation.annotation.Validated;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@Validated
+public class Address {
+    private String street;
+    private String houseNumber;
+    private String zipCode;
+}
